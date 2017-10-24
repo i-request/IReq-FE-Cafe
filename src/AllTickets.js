396d25077593b5db60b365fb6118c65397b16a91
pokerdice <pokerdice@outlook.com>
2017-10-24 18:14:32 +0100

    update cards with new object features, and flow left > right

--- src/AllTickets.js
+++ src/AllTickets.js
@@ -2,62 +2,43 @@ import React, {Component} from 'react';
 
 class AllTickets extends Component {
   render() {
-    console.log(this.props.order)
     return (
-      <div>
-        {this.props.orders.map((item, i, arr) => {
-
+      <section class="container-fluid">
+      <div class="row">
+        {this.props.tickets.map((item, i, arr) => {
           return (
-            <div>
-
-              <div className="test uk-card uk-card-default uk-width-1-5@m">
-                <div className="uk-card-header">
-                  <div className="uk-grid-small uk-flex-middle uk-grid">
-                    <div className="uk-width-auto">
-                      <img width="60" height="60" src="http://www.myunox.com/cms/wp-content/uploads/2013/03/coop.jpg"/>
-                    </div>
-                    <div className="uk-width-expand">
-                      <h3 className="uk-card-title uk-margin-remove-bottom">{item.user_details.user_name} <br /> Order Id: {item.order_id}</h3>
-                    </div>
-                  </div>
-                </div>
-                <div className="uk-card-body">
-                  <table className="uk-table">
-
-                    <caption className="caption-style">Additional Instructions <br /> <p>{item.additional_instructions.message}</p></caption>
-
-                    <thead>
-                      <tr>
-                        <th className="ticket-header">Product</th>
-                        <th className="ticket-header">Size</th>
-                        <th className="ticket-header">Quantity</th>
+            <div key={i}>
+              <div className="card rounded border border-primary mb-3 card-margin">
+              <div className="card-header" key={i}><img className="rounded" width="40" height="40" alt='Company Logo' hspace="5" src={`/images/${item.user_details.user_company_img}`}/><span className="left align-top padded">{item.user_details.user_name}</span><span className="right">Order ID: <strong>{item._id}</strong></span></div>
+              <div className="card-block">
+              <table className="uk-table">
+                <caption className="caption-style" key={i}>Additional Instructions<br />{item.additional_instructions}</caption>
+                <thead>
+                  <tr>
+                    <td className="ticket-header">Order</td>
+                    <td className="ticket-header">Extras</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {item.order_content.map((ticket, index) => {
+                    return (
+                      <tr key={index}>
+                        <td className="order-details">{ticket.name}</td>
+                        <td className="order-details">{item.order_content[index].extras}</td> 
                       </tr>
-                    </thead>
-                    <tbody>
-
-                      {item.order_content.map((order, index) => {
-                        return (
-                          <tr>
-                            <td className="order-details" key={index}>{order.item.name}</td>
-                            <td className="order-details" key={index}>{order.size}</td>
-                            <td className="order-details" key={index}>{order.quantity}</td>
-                          </tr>
-                        )
-                      })
-                     }
-                    </tbody>
-                  </table>
-                  <ul className="uk-list uk-list-striped"></ul>
-                </div>
-                <div className="uk-card-footer">
-                  <a href="#" className="uk-button uk-button-text">Read more</a>
-                </div>
+                    )
+                  })
+                 }
+                </tbody>
+              </table>
+              <div class="card-footer bg-transparent border-primary"><a href="#" className="btn btn-primary">Viewed</a> <a href="#" className="btn btn-danger right">Done</a></div>
               </div>
-
+            </div>
             </div>
           )
         })}
       </div>
+      </section>
     )
   }
 }
--- src/App.js
+++ src/App.js
@@ -7,48 +7,190 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      orders: [
-        { //start of order obj
-          order_id: 1,
-          order_num: 10,
-          order_complete: false,
-          order_viewed: false,
-          paid: false,
-          order_content: [ //user order (basket content)
-          {//ITEM 1
-              item: {
-                type: "food",   //type of item in basket
-                name: "burger", //name of item in basket
-                product_id: 100 //product id of item in basket
-              },
-            size : "sm", //size of item added to basket
-            quantity : 1,//quantity of items order for specific item
-            price : 2 //individual price of item in basket
-          },
-          {//ITEM 2
-            item: {
-              type: "drink",
-              name: "coffee",
-              product_id: 90
+      tickets :[
+        { _id: "mongoID",
+          order_num: 5,
+          isComplete: false,
+          isViewed: false,
+          isCanceled: false,
+          delivery: false,
+          order_content: [ 
+            {
+              _id: "mongoID",
+              type: "food",
+              name: "Super hot dog",
+              extras: ["cheese", "ketchup", "catsup"],
+              price: 500,
+              inStock: true,
+              allergens: ["meat", "dairy", "egg"]
             },
-            size: "xl",
-            quantity: 1,
-            price: 1
+            {
+            _id: "mongoID",
+            type: "food",
+            name: "Panini (ham and cheese)",
+            extras: ["ketchup"],
+            price: 550,
+            inStock: true,
+            allergens: ["meat", "dairy", "egg"]
+            },
+                        {
+            _id: "mongoID",
+            type: "drink",
+            name: "Pepsi",
+            extras: [""],
+            price: 50,
+            inStock: true,
+            allergens: [""]
+            }
+          ],
+          additional_instructions: "test message",
+          user_details : {
+            id : "mongoID",
+            user_name: "Joe Bloggs",
+            email : "joebloggs@hotmail.com", 
+            phone_num : "01617991075", 
+            user_company : "Co-Op",
+            user_company_img : "co-op.png",
+            user_floor : 5
           }
-        ],//CLOSE ORDER CONTENT
-        additional_instructions: {
-          message: 'test message'
         },
-        user_details : {
-            id : 1, //user id
-            user_name: "Jonathan Ward",
-            email : "jonathan@forwardmarketingonline.co.uk", //user email
-            phone_num : "01617991075", //user contact number
-            user_company : "co-op", //user company
-            user_floor : 5, //floor user is on
-            allergens : {nuts : true, dairy : false, gluten : false} //user allergens
+        { _id: "mongoID",
+          order_num: 6,
+          isComplete: false,
+          isViewed: false,
+          isCanceled: false,
+          delivery: true,
+          order_content: [ 
+            {
+              _id: "mongoID",
+              type: "food",
+              name: "Sausage roll",
+              extras: ["ketchup"],
+              price: 100,
+              inStock: true,
+              allergens: ["meat", "dairy", "egg"]
+            },
+            {
+            _id: "mongoID",
+            type: "drink",
+            name: "Filter coffee",
+            extras: ["milk"],
+            price: 200,
+            inStock: true,
+            allergens: ["dairy"]
+            }
+          ],
+          additional_instructions: "test message",
+          user_details : {
+            id : "mongoID",
+            user_name: "Jane Doe",
+            email : "jane.doe@gmail.com", 
+            phone_num : "07977123456", 
+            user_company : "Silverchip",
+            user_company_img : "silverchip.png",
+            user_floor : 2
           }
-        } //end of order obj
+        },{ _id: "mongoID",
+        order_num: 5,
+        isComplete: false,
+        isViewed: false,
+        isCanceled: false,
+        delivery: false,
+        order_content: [ 
+          {
+            _id: "mongoID",
+            type: "food",
+            name: "Super hot dog",
+            extras: ["cheese"],
+            price: 500,
+            inStock: true,
+            allergens: ["meat", "dairy", "egg"]
+          },
+          {
+          _id: "mongoID",
+          type: "food",
+          name: "Panini (ham and cheese)",
+          extras: ["ketchup"],
+          price: 550,
+          inStock: true,
+          allergens: ["meat", "dairy", "egg"]
+          },
+                      {
+          _id: "mongoID",
+          type: "drink",
+          name: "Pepsi",
+          extras: [""],
+          price: 50,
+          inStock: true,
+          allergens: [""]
+          },
+          {
+            _id: "mongoID",
+            type: "drink",
+            name: "Breakfast Tea",
+            extras: ["milk"],
+            price: 50,
+            inStock: true,
+            allergens: [""]
+            }
+        ],
+        additional_instructions: "test message",
+        user_details : {
+          id : "mongoID",
+          user_name: "Joe Bloggs",
+          email : "joebloggs@hotmail.com", 
+          phone_num : "01617991075", 
+          user_company : "Co-Op",
+          user_company_img : "co-op.png",
+          user_floor : 5
+        }
+      },
+      { _id: "mongoID",
+        order_num: 6,
+        isComplete: false,
+        isViewed: false,
+        isCanceled: false,
+        delivery: true,
+        order_content: [ 
+          {
+            _id: "mongoID",
+            type: "food",
+            name: "Sausage roll",
+            extras: ["ketchup"],
+            price: 100,
+            inStock: true,
+            allergens: ["meat", "dairy", "egg"]
+          },
+          {
+          _id: "mongoID",
+          type: "drink",
+          name: "Filter coffee",
+          extras: ["milk"],
+          price: 200,
+          inStock: true,
+          allergens: ["dairy"]
+          },
+          {
+            _id: "mongoID",
+            type: "drink",
+            name: "Breakfast Tea",
+            extras: ["milk"],
+            price: 50,
+            inStock: true,
+            allergens: [""]
+            }
+        ],
+        additional_instructions: "test message",
+        user_details : {
+          id : "mongoID",
+          user_name: "Jane Doe",
+          email : "jane.doe@gmail.com", 
+          phone_num : "07977123456", 
+          user_company : "Silverchip",
+          user_company_img : "silverchip.png",
+          user_floor : 2
+        }
+      }
       ]
     }
   }
@@ -56,7 +198,7 @@ render() {
   return (
     <div>
       <NavBar />
-      <AllTickets orders={this.state.orders}/>
+      <AllTickets tickets={this.state.tickets}/>
     </div>
   );
 }
