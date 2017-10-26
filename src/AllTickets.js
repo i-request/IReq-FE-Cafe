7d7395bc61c597820d993053411ff15e8e09d062
pokerdice <pokerdice@outlook.com>
2017-10-26 10:58:59 +0100

    View and Done buttons send change to server

--- src/AllTickets.js
+++ src/AllTickets.js
@@ -9,11 +9,15 @@ class AllTickets extends Component {
     this.fetchTickets = this.fetchTickets.bind(this)
     this.handleViewed = this.handleViewed.bind(this)
     this.handleDone = this.handleDone.bind(this)
+    this.updateViewData = this.updateViewData.bind(this)
     this.state = {
       tickets: []
     }
   }
-
+  componentDidMount() {
+    this.fetchTickets()
+  }
+  
   renderView(bool) {
     const colorClass = bool
       ? 'success'
@@ -21,8 +25,6 @@ class AllTickets extends Component {
     const viewIcon = bool
       ? 'check'
       : 'times';
-    // const positve = <i class="fa fa-check" aria-hidden="true" />
-    // const negative = <i class="fa fa-times" aria-hidden="true"></i> />
     return (
       <button id='viewBtn' className={`btn btn-${colorClass}`}>Viewed
         <i className={`fa fa-${viewIcon}`} aria-hidden="true"></i>
@@ -46,41 +48,61 @@ class AllTickets extends Component {
 
   }
 
+  updateViewData(id) { 
+    const trueId = id.toString()
+    return axios.put(`http://localhost:9007/tickets/${trueId}?isViewed=true`)
+      .then(res => {
+      // console.log(res)
+      })
+      .catch(console.log)
+  };
 
-  componentDidMount() {
-    this.fetchTickets()
-  }
+  updateDoneData(id, bool) { 
+    const trueId = id.toString()
+    return axios.put(`http://localhost:9007/tickets/${trueId}?isComplete=${bool}`)
+      .then(res => {
+      // console.log(res)
+      })
+      .catch(console.log)
+  };
+
   fetchTickets() {
-    return axios.get('http://localhost:9007/tickets').then(res => {
-      console.log(res)
+    return axios.get('http://localhost:9007/tickets')
+      .then(res => {
+      // console.log(res)
       this.setState({tickets: res.data})
-    }).catch(console.log)
+      })
+      .catch(console.log)
   }
+
   handleViewed(orderNum) {
+    const t = this;
     return (e) => {
-      var newState = this.state.tickets.map(function(item) {
+      let newState = this.state.tickets.map((item) => {
         if (item.order_num === orderNum) {
           item.isViewed = true
-        }
+          t.updateViewData(item._id)
+       }
         return item;
       })
       this.setState({tickets: newState})
+   }
+ }
 
-    }
 
-  }
+
   handleDone(orderNum) {
+    const t = this;
     return (e) => {
-      var newState = this.state.tickets.map(function(item) {
+      let newState = this.state.tickets.map((item) => {
         if (item.order_num === orderNum) {
           item.isComplete = !item.isComplete
+          t.updateDoneData(item._id, item.isComplete)
         }
         return item;
       })
       this.setState({tickets: newState})
-
     }
-
   }
 
 
@@ -93,22 +115,24 @@ class AllTickets extends Component {
             return (
 
               <div className="card bg-light mb-3 tickets" key={i}>
-                <div className="card-header" key={i}><img className="rounded" width="40" height="40" alt='Company Logo' hspace="5" src={`/images/${item.user_details.user_company_img}`}/>
+                <div className="card-header" key={i}><img className="rounded" width="40" height="40" alt='Company Logo' hspace="5" />
                   <span className="left align-top padded">{item.user_details.user_name}</span>
                   <span className="right">Order ID:
                     <strong>{item.order_num}</strong>
                   </span>
                 </div>
                 <div className="card-body test">
+                  <ul>
                 {item.order_content.map((ticket, index) => {
                   return (
-                    <p className="card-text" key={index}>{ticket.name}</p>
+                    <li className="card-text" key={index}>{ticket.name}</li>
                   )
                 })}
-
+                    </ul>
                   <div className="test1">
                     <span id='viewBtn' onClick={this.handleViewed(item.order_num)}>{this.renderView(item.isViewed)}</span>
                     <span id="doneBtn" onClick={this.handleDone(item.order_num)}>{this.renderDone(item.isComplete)}</span>
+
                   </div>
                 </div>
                 </div>
