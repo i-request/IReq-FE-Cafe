3cb48e243dd637d25178d89e9459efe316ebf5fd
Jonny Ward <JonnyWard@Macs-MacBook-Pro.local>
2017-10-26 08:57:40 +0100

    refactored tickets/need to fix positioning of buttons on tickets

--- src/AllTickets.js
+++ src/AllTickets.js
@@ -24,7 +24,7 @@ class AllTickets extends Component {
     // const positve = <i class="fa fa-check" aria-hidden="true" />
     // const negative = <i class="fa fa-times" aria-hidden="true"></i> />
     return (
-      <button id='viewBtn' className={`btn left btn-${colorClass}`}>Viewed
+      <button id='viewBtn' className={`btn btn-${colorClass}`}>Viewed
         <i className={`fa fa-${viewIcon}`} aria-hidden="true"></i>
       </button>
     );
@@ -39,7 +39,7 @@ class AllTickets extends Component {
       ? 'check'
       : 'times';
     return (
-      <button id='doneBtn' className={`btn right btn-${doneColorClass}`}>Done
+      <button id='doneBtn' className={`btn btn-${doneColorClass}`}>Done
         <i className={`fa fa-${doneIcon}`} aria-hidden="true"></i>
       </button>
     );
@@ -89,56 +89,42 @@ class AllTickets extends Component {
       <section className="container-fluid">
         <div className="row">
           {this.state.tickets.map((item, i, arr) => {
+
             return (
-              <div className="tickets" key={i}>
-                <div className="card rounded border border-primary mb-3 card-margin">
-                  <div className="card-header" key={i}><img className="rounded" width="40" height="40" alt='Company Logo' hspace="5" src={`/images/${item.user_details.user_company_img}`}/>
-                    <span className="left align-top padded">{item.user_details.user_name}</span>
-                    <span className="right">Order ID:
-                      <strong>{item.order_num}</strong>
-                    </span>
-                  </div>
-                  <div className="card-block">
-                    <table className="uk-table">
 
-                      <thead>
-                        <tr>
-                          <td className="ticket-header">Order</td>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {item.order_content.map((ticket, index) => {
-                          return (
-                            <tr key={index}>
-                              <td className="order-details">{ticket.name}</td>
+              <div className="card bg-light mb-3 tickets" key={i}>
+                <div className="card-header" key={i}><img className="rounded" width="40" height="40" alt='Company Logo' hspace="5" src={`/images/${item.user_details.user_company_img}`}/>
+                  <span className="left align-top padded">{item.user_details.user_name}</span>
+                  <span className="right">Order ID:
+                    <strong>{item.order_num}</strong>
+                  </span>
+                </div>
+                <div className="card-body test">
+                {item.order_content.map((ticket, index) => {
+                  return (
+                    <p className="card-text" key={index}>{ticket.name}</p>
+                  )
+                })}
 
-                            </tr>
-                          )
-                        })
-                      }
-                      </tbody>
-                      <div className="card-footer bg-transparent border-primary">
-                        <a href="#" id='viewBtn'>
-                          <span onClick={this.handleViewed(item.order_num)}>{this.renderView(item.isViewed)}</span>
-                        </a>
-                        <a href="#" id="doneBtn">
-                          <span onClick={this.handleDone(item.order_num)}>{this.renderDone(item.isComplete)}</span>
-                        </a>
-
-                      </div>
-                    </table>
+                  <div className="test1">
+                    <span id='viewBtn' onClick={this.handleViewed(item.order_num)}>{this.renderView(item.isViewed)}</span>
+                    <span id="doneBtn" onClick={this.handleDone(item.order_num)}>{this.renderDone(item.isComplete)}</span>
                   </div>
                 </div>
-              </div>
-            )
-          })}
-        </div>
-      </section>
+                </div>
+
+
 
 
 
     )
   }
+)}
+
+</div>
+</section>
+)
+}
 }
 export default AllTickets
 
