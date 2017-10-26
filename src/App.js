130e2258136a2903ae08512beedd1074c5ab2e06
Jonny Ward <JonnyWard@Macs-MacBook-Pro.local>
2017-10-26 13:20:35 +0100

    added new tabs to navbar, added functionality to pending tickets

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AllTickets from './AllTickets';
 import Admin from './Admin';
 import Archived from './Archived';
 import Cancelled from './Cancelled';
-import OrderType from './OrderType';
+import PendingTickets from './PendingTickets';
 import Trash from './Trash';
 import axios from 'axios';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
@@ -13,16 +13,16 @@ import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 class App extends Component {
 
   render() {
-    return (
 
+    return (
         <Router>
         <div>
           <NavBar/>
-            <Route exact path="/" component={Admin}/>
+            <Route exact path="/" component={PendingTickets}/>
             <Route path="/all-tickets" component={AllTickets}/>
             <Route path="/archived" component={Archived}/>
             <Route path="/cancelled" component={Cancelled}/>
-            <Route path="/order-type" component={OrderType}/>
+            <Route path="/admin" component={Admin}/>
             <Route path="/trash" component={Trash}/>
           </div>
         </Router>
--- src/PendingTickets.js
+++ src/PendingTickets.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios'
-
-class AllTickets extends Component {
+class OrderType extends Component {
   constructor(props) {
     super(props)
     this.renderView = this.renderView.bind(this)
@@ -16,6 +15,13 @@ class AllTickets extends Component {
   }
   componentDidMount() {
     this.fetchTickets()
+  }
+
+  fetchTickets() {
+    return axios.get('http://localhost:9007/tickets?isComplete=false').then(res => {
+      console.log(res)
+      this.setState({tickets: res.data})
+    }).catch(console.log)
   }
 
   renderView(bool) {
@@ -98,41 +104,41 @@ class AllTickets extends Component {
   }
 
   render() {
-    console.log(this.state.tickets)
     return (
       <section className="container-fluid">
         <div className="row">
-          {this.state.tickets.map((item, i, arr) => {
-
+        {this.state.tickets.map((item, i, arr) => {
+          if (item.isComplete === false) {
             return (
-
-              <div className="card bg-light mb-3 tickets" key={i}>
-                <div className="card-header" key={i}><img className="rounded" width="40" height="40" alt='Company Logo' hspace="5"/>
-                  <span className="left align-top padded">{item.user_details.user_name}</span>
-                  <span className="right">Order ID:
-                    <strong>{item.order_num}</strong>
-                  </span>
-                </div>
-                <div className="card-body viewed-done-flex-container">
-                  <ul>
-                    {item.order_content.map((ticket, index) => {
-                      return (
-                        <li className="card-text" key={index}>{ticket.name}</li>
-                      )
-                    })}
-                  </ul>
-                  <div className="viewed-done-button-container">
-                    <span id='viewBtn' onClick={this.handleViewed(item.order_num)}>{this.renderView(item.isViewed)}</span>
-                    <span id="doneBtn" onClick={this.handleDone(item.order_num)}>{this.renderDone(item.isComplete)}</span>
+              <div>
+                <div className="card bg-light mb-3 tickets" key={i}>
+                  <div className="card-header" key={i}><img className="rounded" width="40" height="40" alt='Company Logo' hspace="5"/>
+                    <span className="left align-top padded">{item.user_details.user_name}</span>
+                    <span className="right">Order ID:
+                      <strong>{item.order_num}</strong>
+                    </span>
+                  </div>
+                  <div className="card-body viewed-done-flex-container">
+                    <ul>
+                      {item.order_content.map((ticket, index) => {
+                        return (
+                          <li className="card-text" key={index}>{ticket.name}</li>
+                        )
+                      })}
+                    </ul>
+                    <div className="viewed-done-button-container">
+                      <span id='viewBtn' onClick={this.handleViewed(item.order_num)}>{this.renderView(item.isViewed)}</span>
+                      <span id="doneBtn" onClick={this.handleDone(item.order_num)}>{this.renderDone(item.isComplete)}</span>
+                    </div>
                   </div>
                 </div>
               </div>
             )
-          })}
-
+          }
+        })}
         </div>
       </section>
     )
   }
 }
-export default AllTickets
+export default OrderType
