8edf947906619892526080f4dfa701539d56cccc
Jonny Ward <JonnyWard@Macs-MacBook-Pro.local>
2017-10-27 07:52:56 +0100

    tickets updated, user details added

--- src/AllTickets.js
+++ src/AllTickets.js
@@ -4,15 +4,15 @@ import axios from 'axios'
 class AllTickets extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      tickets: []
+    }
     this.renderView = this.renderView.bind(this)
     this.renderDone = this.renderDone.bind(this)
     this.fetchTickets = this.fetchTickets.bind(this)
     this.handleViewed = this.handleViewed.bind(this)
     this.handleDone = this.handleDone.bind(this)
     this.updateViewData = this.updateViewData.bind(this)
-    this.state = {
-      tickets: []
-    }
   }
   componentDidMount() {
     this.fetchTickets()
@@ -57,15 +57,15 @@ class AllTickets extends Component {
 
   updateDoneData(id, bool) {
     const trueId = id.toString()
-    return axios.put(`http://localhost:9007/tickets/${trueId}?isComplete=${bool}`).then(res => {
-      // console.log(res)
+    return axios.put(`http://localhost:9007/tickets/${trueId}?isComplete=${bool}`).then(result => {
+      // console.log(result)
     }).catch(console.log)
   };
 
   fetchTickets() {
-    return axios.get('http://localhost:9007/tickets').then(res => {
-      // console.log(res)
-      this.setState({tickets: res.data})
+    return axios.get('http://localhost:9007/tickets').then(result => {
+      // console.log(result)
+      this.setState({tickets: result.data})
     }).catch(console.log)
   }
 
@@ -98,26 +98,55 @@ class AllTickets extends Component {
   }
 
   render() {
-    console.log(this.state.tickets)
     return (
       <section className="container-fluid">
         <div className="row">
           {this.state.tickets.map((item, i, arr) => {
-
+            console.log(item)
             return (
-
               <div className="card bg-light mb-3 tickets" key={i}>
-                <div className="card-header" key={i}><img className="rounded" width="40" height="40" alt='Company Logo' />
+                <div className="card-header" key={i}><img className="rounded" src="../public/images/co-op.png" width="40" height="40" alt='Company Logo'/>
                   <span className="left align-top padded">{item.user_details.user_name}</span>
                   <span className="right">Order ID:
                     <strong>{item.order_num}</strong>
                   </span>
                 </div>
                 <div className="card-body viewed-done-flex-container">
-                  <ul>
-                    {item.order_content.map((ticket, index) => {
+                  <ul className="ticket-ul-style">
+                    <h4 className="ticket-order-header">Order</h4>
+                    <div className="container-fluid">
+                      <div className="row">
+                        <div className="col-md-9 remove-left-padding">
+                          <h6 className="ticket-product-headers">Product</h6>
+                        </div>
+                        <div className="col-md-3 remove-right-padding">
+                          <h6 className="ticket-product-headers">QTY</h6>
+                        </div>
+                      </div>
+                    </div>
+                    {item.order_content.map((element, index) => {
+                      var e = 'none'
+                      if (element.extras.length > 0) {
+                        e = element.extras.toString()
+                      }
                       return (
-                        <li className="card-text" key={index}>{ticket.name}</li>
+                        <div>
+                          <li className="card-text" key={index}>{element.name}</li>
+                          <p className="extras">extras: {e}</p>
+                        </div>
+                      )
+                    })}
+                    <h4 className="ticket-user-details-header">User Details</h4>
+                    {item.user_details.map((user, index) => {
+                      console.log(user)
+                      return (
+                        <div>
+                          <li className="card-text" key={index}>Name: {user.user_name}</li>
+                          <li className="card-text" key={index}>Email: {user.email}</li>
+                          <li className="card-text" key={index}>Number: {user.phone_num}</li>
+                          <li className="card-text" key={index}>Company: {user.user_company}</li>
+                          <li className="card-text" key={index}>Floor: {user.user_floor}</li>
+                        </div>
                       )
                     })}
                   </ul>
--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App extends Component {
             <Route path="/trash" component={Trash}/>
           </div>
         </Router>
-
     );
   }
 }
