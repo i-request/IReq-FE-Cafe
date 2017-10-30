261f91b08aa94cbfd783ecb85bb984f1739e6460
Jonny Ward <JonnyWard@Macs-MacBook-Pro.local>
2017-10-30 17:13:54 +0000

    made changes to all ticket functionality/refactored

--- src/PendingTickets.js
+++ src/PendingTickets.js
@@ -1,45 +1,7 @@
-import React, {Component} from 'react';
-import axios from 'axios'
+import React from 'react';
 import TicketList from './TicketList';
 
-class PendingTickets extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      tickets: []
-    }
-    this.fetchTickets = this.fetchTickets.bind(this);
-  }
-  fetchTickets() {
-    return axios.get('http://localhost:9007/tickets').then(result => {
-      // console.log(result)
-    this.setState({tickets: result.data})
-    }).catch(console.log)
-  }
-
-  componentDidMount() {
-    this.fetchTickets();
-  };
-
-  isComplete() {
-    let pending = []
-    this.state.tickets.map((pendingTicket) => {
-      pendingTicket.isComplete === false ? pending.push(pendingTicket) : ''
-    })
-    console.log(pending)
-  }
-
-
-  render() {
-    return (
-      <div>
-      {this.state.tickets.map((ticket) => {
-        let isComplete = ticket.isComplete;
-        console.log(isComplete)
-        if(!isComplete) return <TicketList />
-      })}
-      </div>
-    )
-  }
+const PendingTickets = () => {
+  return <TicketList parm1={'isComplete'} bool={false} />
 }
 export default PendingTickets
